0024970de6216f37f5b3cec994f91ae7f093a13d
Maya Chang <mcc9@hawaii.edu>
2023-04-18 09:25:34 -1000

    fixed landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, Container } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <div>
-    <Row className="align-middle text-center vh-100 w-100">
+  <Container fluid>
+    <Row className="d-flex align-middle text-center vh-100">
       <Col xs={8} id="landing-bg-1" className="border border-dark" />
       <Col xs={4} className="d-flex flex-column justify-content-center border border-dark" id="landing-bg-words">
         <h1>Welcome to Manoa Recipes!</h1>
       </Col>
     </Row>
-    <Row className="align-middle text-center vh-100 w-100">
+    <Row className="d-flex align-middle text-center vh-100">
       <Col xs={4} className="d-flex flex-column justify-content-center border border-dark" id="landing-bg-words">
         <h1>Learn New College-Student-Friendly Recipes</h1>
       </Col>
       <Col xs={8} id="landing-bg-2" className="border border-dark" />
     </Row>
-    <Row className="align-middle text-center vh-100 w-100">
+    <Row className="d-flex align-middle text-center vh-100">
       <Col xs={8} id="landing-bg-3" className="border border-dark" />
       <Col xs={4} className="d-flex flex-column justify-content-center border border-dark" id="landing-bg-words">
         <h1>Share Your Own Recipes</h1>
       </Col>
     </Row>
-  </div>
+  </Container>
 );
 
 export default Landing;
