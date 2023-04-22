ff2d15192800ece89ead695b4ed8e287483af3b0
chenglisa <97767296+chenglisa@users.noreply.github.com>
2023-04-22 11:37:42 -1000

    make recipe card link to individual recipe

--- app/imports/ui/components/RecipeCard.jsx
+++ app/imports/ui/components/RecipeCard.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Image } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import { Clock } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const RecipeCard = ({ recipe }) => (
-  <Card style={{ width: '18rem' }} className="h-100">
-    <Card.Img variant="top" src={recipe.image} style={{ height: '40vh' }} />
-    <Card.Title className="px-3">{recipe.name}</Card.Title>
-    <Card.Subtitle className="px-3">{recipe.owner}</Card.Subtitle>
-    <Card.Body>
-      <Card.Text>{recipe.instructions}</Card.Text>
-      <Card.Text><Clock /> {recipe.time}</Card.Text>
-    </Card.Body>
-  </Card>
+  <Link to={`/view-recipe/${recipe._id}`}>
+    <Card style={{ width: '18rem' }} className="h-100">
+      <Card.Img variant="top" src={recipe.image} style={{ height: '40vh' }} />
+      <Card.Title className="px-3">{recipe.name}</Card.Title>
+      <Card.Subtitle className="px-3">{recipe.owner}</Card.Subtitle>
+      <Card.Body>
+        <Card.Text>{recipe.instructions}</Card.Text>
+        <Card.Text><Clock /> {recipe.time}</Card.Text>
+      </Card.Body>
+    </Card>
+  </Link>
 
 );
 
