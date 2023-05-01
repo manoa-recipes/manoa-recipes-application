ffa6c612d9dffd741bc885e6204d2965477c43ef
chenglisa <97767296+chenglisa@users.noreply.github.com>
2023-05-01 01:59:25 -1000

    edits

--- app/imports/ui/pages/recipe/ListRecipe.jsx
+++ app/imports/ui/pages/recipe/ListRecipe.jsx
@@ -9,7 +9,7 @@ import { Recipes } from '../../../api/recipes/Recipes';
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const UserHome = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const [restriction, setRestriction] = useState(false);
+  const [restriction, setRestriction] = useState({glutenFree: false, vegan: false });
   const { ready, recipes } = useTracker(() => {
   // Note that this subscription will get cleaned up
   // when your component is unmounted or deps change.
@@ -19,12 +19,16 @@ const UserHome = () => {
     const rdy = subscription.ready();
     // Get the Stuff documents
     const recipesItems = Recipes.collection.find({}).fetch();
+
     return {
       recipes: recipesItems,
       ready: rdy,
     };
   }, []);
 
+  const handleFilterClick = (filterType) => {
+    setRestriction({ ...restriction, [filterType]: !restriction[filterType] });
+  };
   return (ready ? (
     <Container className="py-3" id="list-recipe-page">
       <Row className="justify-content-center">
@@ -33,8 +37,10 @@ const UserHome = () => {
             <h2>All Recipes</h2>
           </Col>
           <Col className="d-flex justify-content-center">
-            <Button className="p-1 px-2 mx-5" variant="btn btn-secondary">Vegan</Button>
-            <Button className="p-1 px-2 mx-5" variant="btn btn-secondary">Gluten-free</Button>
+            <Button className="p-1 px-2 mx-5" variant={restriction.vegan ? 'success' : 'btn btn-secondary'} onClick={() => handleFilterClick('vegan')}>Vegan</Button>
+
+            <Button variant={restriction.glutenFree ? 'success' : 'outline-secondary'} onClick={() => handleFilterClick('glutenFree')}>Gluten Free</Button>
+
           </Col>
           <Row xs={1} md={2} lg={3} className="g-4 mt-1">
             {recipes.map((recipe) => (<Col key={recipe._id}><RecipeCard recipe={recipe} /></Col>))}
