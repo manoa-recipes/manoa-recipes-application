5d93e071bfab0434c46fec286e30a7050a4f7979
chenglisa <97767296+chenglisa@users.noreply.github.com>
2023-05-02 11:05:11 -1000

    fix eslint

--- app/imports/ui/pages/AddProfile.jsx
+++ app/imports/ui/pages/AddProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AutoForm, TextField, SubmitField, ErrorsField } from 'uniforms-bootstrap5';
 import { Container, Col, Card, Form } from 'react-bootstrap';
 import swal from 'sweetalert';
@@ -10,7 +10,6 @@ import { useTracker } from 'meteor/react-meteor-data';
 import { Navigate } from 'react-router-dom';
 import { Profiles } from '../../api/profiles/Profiles';
 import LoadingSpinner from '../components/LoadingSpinner';
-import { Roles } from 'meteor/alanning:roles';
 
 /* Create a schema to specify the structure of the data to appear in the form. */
 const makeSchema = (allAllergies) => new SimpleSchema({
