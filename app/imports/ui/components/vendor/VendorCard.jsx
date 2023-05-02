793b42c85de26faa8db69af1ad553aa54baf7a1c
chenglisa <97767296+chenglisa@users.noreply.github.com>
2023-05-02 05:31:15 -1000

    adjust vendor card

--- app/imports/ui/components/vendor/VendorCard.jsx
+++ app/imports/ui/components/vendor/VendorCard.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import VendorIngredient from './VendorIngredient';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const VendorCard = ({ vendor }) => (
+const VendorCard = ({ vendor, vendorIngredients }) => (
   <Link to="/" className="recipeLink">
     <Card className="h-100">
       <div className="row g-0">
@@ -16,6 +17,8 @@ const VendorCard = ({ vendor }) => (
             <Card.Title>{vendor.name}</Card.Title>
             <Card.Text>Address: {vendor.address}</Card.Text>
             <Card.Text>Hours: {vendor.hours}</Card.Text>
+            <Card.Text>{vendorIngredients.map((vendorIngredient) => <VendorIngredient key={vendorIngredient._id} vendorIngredient={vendorIngredient} />)}
+            </Card.Text>
           </Card.Body>
         </div>
       </div>
@@ -33,6 +36,15 @@ VendorCard.propTypes = {
     image: PropTypes.string,
     _id: PropTypes.string,
   }).isRequired,
+
+  vendorIngredients: PropTypes.arrayOf(PropTypes.shape({
+    address: PropTypes.string,
+    ingredient: PropTypes.string,
+    inStock: PropTypes.bool,
+    size: PropTypes.string,
+    price: PropTypes.number,
+    _id: PropTypes.string,
+  })).isRequired,
 };
 
 export default VendorCard;
--- app/imports/ui/pages/Vendors.jsx
+++ app/imports/ui/pages/Vendors.jsx
@@ -5,21 +5,25 @@ import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
 import VendorCard from '../components/vendor/VendorCard';
 import { Vendors } from '../../api/vendors/Vendors';
+import { VendorsIngredients} from '../../api/vendors/VendorsIngredients';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const VendorList = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { ready, vendors } = useTracker(() => {
+  const { ready, vendors, vendorIngredients } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
     const subscription = Meteor.subscribe(Vendors.userPublicationName);
+    const subscription2 = Meteor.subscribe(VendorsIngredients.userPublicationName);
     // Determine if the subscription is ready
-    const rdy = subscription.ready();
+    const rdy = subscription.ready() && subscription2.ready();
     // Get the Stuff documents
     const vendorList = Vendors.collection.find({}).fetch();
+    const vendorStuff = VendorsIngredients.collection.find({}).fetch();
     return {
       vendors: vendorList,
+      vendorIngredients: vendorStuff,
       ready: rdy,
     };
   }, []);
@@ -33,7 +37,7 @@ const VendorList = () => {
             <h4><i>Click on each store to see what&apos;s in stock.</i></h4>
           </Col>
           <Row xs={1} md={2} lg={3} className="g-4">
-            {vendors.map((vendor) => (<Col key={vendor._id}><VendorCard vendor={vendor} /></Col>))}
+            {vendors.map((vendor) => (<Col key={vendor._id}><VendorCard vendor={vendor} vendorIngredients={vendorIngredients.filter(vendorIngredient => (vendorIngredient.address === vendor.address))} /></Col>))}
           </Row>
         </Col>
       </Row>
