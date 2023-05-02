b380848ac859ea3b3424f9515d498d648a924054
chenglisa <97767296+chenglisa@users.noreply.github.com>
2023-05-02 05:55:05 -1000

    Update VendorCard.jsx

--- app/imports/ui/components/vendor/VendorCard.jsx
+++ app/imports/ui/components/vendor/VendorCard.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Accordion, Card, Table } from 'react-bootstrap';
 import VendorIngredient from './VendorIngredient';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const VendorCard = ({ vendor, vendorIngredients }) => (
-  <Link to="/" className="recipeLink">
-    <Card className="h-100">
-      <div className="row g-0">
-        <div className="col-md-6">
-          <Card.Img src={vendor.image} className="vendorCard" style={{ height: '200px' }} />
-        </div>
-        <div className="col-md-6">
-          <Card.Body>
-            <Card.Title>{vendor.name}</Card.Title>
-            <Card.Text>Address: {vendor.address}</Card.Text>
-            <Card.Text>Hours: {vendor.hours}</Card.Text>
-            <Card.Text>{vendorIngredients.map((vendorIngredient) => <VendorIngredient key={vendorIngredient._id} vendorIngredient={vendorIngredient} />)}
-            </Card.Text>
-          </Card.Body>
-        </div>
+  <Card className="h-100">
+    <div className="row g-0">
+      <div className="col-md-6">
+        <Card.Img src={vendor.image} className="vendorCard" style={{ height: '200px' }} />
       </div>
-    </Card>
-  </Link>
-
+      <div className="col-md-6">
+        <Card.Body>
+          <Card.Title>{vendor.name}</Card.Title>
+          <Card.Text>Address: {vendor.address}</Card.Text>
+          <Card.Text>Hours: {vendor.hours}</Card.Text>
+          <Accordion>
+            <Accordion.Header>
+              <h5>Products</h5>
+            </Accordion.Header>
+            <Accordion.Body>
+              <Table striped bordered variant="light">
+                <thead>
+                  <tr>
+                    <th>Ingredient</th>
+                    <th>In Stock</th>
+                    <th>Size</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {vendorIngredients.map((vendorIngredient) => <VendorIngredient key={vendorIngredient._id} vendorIngredient={vendorIngredient} />)}
+                </tbody>
+              </Table>
+            </Accordion.Body>
+          </Accordion>
+        </Card.Body>
+      </div>
+    </div>
+  </Card>
 );
 
 // Require a document to be passed to this component.
